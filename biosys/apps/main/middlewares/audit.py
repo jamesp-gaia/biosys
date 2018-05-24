0df07c0511f20bc30ee7e410b8cb15e4d5e9f15d
serge <serge@gaiaresources.com.au>
2018-05-24 16:46:17 +0800

    disable atomic in reversion middleware.

--- biosys/apps/main/middlewares/audit.py
+++ biosys/apps/main/middlewares/audit.py
@@ -4,6 +4,11 @@ from reversion.middleware import RevisionMiddleware
 class AuditMiddleware(RevisionMiddleware):
     """
     This class is in charge og auditing(logging) changes in models.
-    As of 24/05/2018 it does nothing more than the reversion middleware.
+    As of 24/05/2018 it does nothing more than the reversion middleware but with the atomic turned off.
+    Setting the atomic to True make one unit test fails
+     test_species_observation.TestSpeciesNameFromNameID.test_wrong_id_rejected_upload
+    Also there's potential issue if the django ATOMIC_REQUESTS is not set to True.
+    see: https://github.com/etianen/django-reversion/issues/624#issuecomment-362413142
     """
-    pass
+    atomic = False
+
